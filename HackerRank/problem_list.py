4c18fa58e6817a0f9afd1dddd87cf06e72fbc296
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-11-05 19:59:21 +0100

    moved file operations to new file

--- HackerRank/problem_list.py
+++ HackerRank/problem_list.py
@@ -2,10 +2,9 @@ import os
 import subprocess
 import requests
 from datetime import datetime as DateTime  # for setting timeout
-from internet import open_browser, get_problem_link_URL # for opening browser
-import time # for determinating file creation time
-import re # for sorting files
-from pathlib import Path # for sorting files
+from internet import open_browser  # for opening browser
+from file import get_code_files, get_problem_link
+import time  # for determinating file creation time
 
 FOLDER = os.path.dirname(os.path.abspath(__file__))
 FOLDER = FOLDER.replace("\\", '/')
@@ -39,146 +38,6 @@ class HackerRankProblem():
         self.difficulty = difficulty
         self.solved_date = solved_date
         self.instruction = instruction
-
-
-def file_lines(file_path):
-    """
-    Counts the number of lines of a file using buffered count
-    :string fname: path to file
-    :return noLines: number of lines in file
-    """
-    if os.path.isfile(file_path):
-        def _make_gen(reader):
-            b = reader(2 ** 16)
-            while b:
-                yield b
-                b = reader(2 ** 16)
-
-        with open(file_path, "rb") as f:
-            count = sum(buf.count(b"\n") for buf in _make_gen(f.raw.read))
-        no_lines = count + 1
-        return no_lines
-    else:
-        return 0
-
-
-def get_code_files():
-    """
-    Gets file paths of code files in all subdirectories
-    :returns: list of file paths
-    """
-    print("Get file paths...")
-    process = subprocess.Popen("git pull", stdout=subprocess.PIPE)
-    result = list()
-    no_files = 0
-    file_extensions = ['.java', '.py']
-
-    def sort_logically(result):
-        """
-        Sorts list like 1, 2, 10 instead of 1, 10, 2
-        :list result: list to sort
-        :returns: sorted list
-        """
-        result2 = list()
-        for item in result:
-            path = Path(item)
-            result2.append(path)
-
-        result = sorted(result2, key = lambda x: [int(k) if k.isdigit() else k for k in re.split('([0-9]+)', x.stem)])
-        for item in result:
-            result[result.index(item)] = str(item)
-        return result
-
-    for _, _, files in os.walk(PROBLEMS):
-        for entry in files:
-            for ext in file_extensions:
-                if entry.endswith(ext):
-                    no_files += 1
-
-    if (os.path.exists(FILE_LIST) and file_lines(FILE_LIST) == no_files):
-        with open(FILE_LIST, 'r') as f:
-            for line in f:
-                line = line.replace('\n', '')
-                result.append(line)
-    else:
-        for root, _, files in os.walk(PROBLEMS):
-            for entry in files:
-                for ext in file_extensions:
-                    if entry.endswith(ext):
-                        entry = os.path.join(root, entry)
-                        entry = entry.replace('\\', '/')
-                        result.append(entry)
-        
-        result = sort_logically(result)
-        with open(FILE_LIST, 'w') as f:
-            for res in result:
-                f.write(res + '\n')
-
-    return result
-
-
-def check_problem_links():
-    """
-    Reads problem link from each code file
-    :returns: list of links, errors
-    """
-    file_paths = get_code_files()
-    links = list()
-    errors = list()
-
-    for file_path in file_paths:
-        index = file_paths.index(file_path)
-        link, success = get_problem_link(file_path, index)
-        if success == True:
-            links.append(link)
-        else:
-            errors.append(file_path)
-    if len(errors) == len(file_paths):
-        raise Exception("Fetching the links failed for all problems")
-    return links, errors
-
-
-def get_problem_link(file_path, index):
-    """
-    Gets the problem link from a HackerRank code file
-    """
-    lines = []
-    if file_lines(file_path) > 1 and not open_replace_newlines() == None:
-        lines = open_replace_newlines(file_path)
-        first_line = lines[0]
-        if first_line == '#!/bin/python3':
-            if not lines[1].startswith('#https://www.hackerrank.com/'):
-                link, success = get_problem_link_URL(file_path, index)
-                link = None
-                success = False
-                return link, success
-        else:  # first line is link
-            link = first_line
-        link = link[1:]  # remove comment sign ('#') from link
-        success = True
-        return link, success
-    else:  # file has only one line
-        link = None
-        success = False
-        return link, success
-
-
-def open_replace_newlines(file_path):
-    """
-    Opens a file and returns list of lines
-    :returns: list of lines or None if error occured
-    """
-    lines = list()
-    try:
-        with open(file_path, 'r') as f:
-            for line in f:
-                lines.append(line)
-        for line in lines:
-            lines[lines.index(line)] = line.replace('\n', '')
-    except IOError:
-        return None
-    return lines
-
 
 
 def process_problems():
@@ -243,7 +102,8 @@ def get_solved_date(file):
     :returns: solved date (int)
     """
     if DATE == 'creation':
-        (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(file)
+        (mode, ino, dev, nlink, uid, gid, size,
+         atime, mtime, ctime) = os.stat(file)
         return str(time.ctime(mtime))
     if DATE == 'last modified':
         process = subprocess.Popen("git pull", stdout=subprocess.PIPE)
