e6a3ad457088fd4efcb7df7dd89a7f8cc5ff5762
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-11-08 19:21:37 +0100

    error corrections

--- HackerRank/file.py
+++ HackerRank/file.py
@@ -4,11 +4,13 @@ import subprocess
 import sys
 from pathlib import Path  # for sorting files
 
-from global_vars import FOLDER, PROBLEMS
-from internet import get_problem_link_URL
+from global_vars import FOLDER, PROBLEMS, DIFFICULTY_PROMPT
+from internet import get_problem_link_HTML
 
 FILE_LIST = FOLDER + '/' + 'file_list.txt'
-FILEUTIL_PATH = '.\\..\\..\\Misc\\'
+MISC_PATH = '.\\..\\..\\Misc\\'
+LINK_LINE_NUMBER = 2 # link should be written on second line of file
+DIFFICULTY_LINE_NUMBER = 3 # difficulty should be written on third line of file
 
 
 def import_module(file_path):
@@ -40,27 +42,6 @@ def import_module(file_path):
     sys.path.append(final_path)
 
 
-def file_lines(file_path):
-    """
-    Counts the number of lines of a file using buffered count
-    :string fname: path to file
-    :returns: number of lines in file
-    """
-    if os.path.isfile(file_path):
-        def _make_gen(reader):
-            b = reader(2 ** 16)
-            while b:
-                yield b
-                b = reader(2 ** 16)
-
-        with open(file_path, "rb") as f:
-            count = sum(buf.count(b"\n") for buf in _make_gen(f.raw.read))
-        no_lines = count + 1
-        return no_lines
-    else:
-        return 0
-
-
 def get_code_files():
     """
     Gets file paths of code files in all subdirectories
@@ -72,30 +53,16 @@ def get_code_files():
     no_files = 0
     file_extensions = ['.java', '.py']
 
-    def sort_logically(result):
-        """
-        Sorts list like 1, 2, 10 instead of 1, 10, 2
-        :list result: list to sort
-        :returns: sorted list
-        """
-        result2 = list()
-        for item in result:
-            path = Path(item)
-            result2.append(path)
-
-        result = sorted(result2, key=lambda x: [
-                        int(k) if k.isdigit() else k for k in re.split('([0-9]+)', x.stem)])
-        for item in result:
-            result[result.index(item)] = str(item)
-        return result
-
     for _, _, files in os.walk(PROBLEMS):
         for entry in files:
             for ext in file_extensions:
                 if entry.endswith(ext):
                     no_files += 1
 
-    if (os.path.exists(FILE_LIST) and file_lines(FILE_LIST) == no_files):
+    import_module(MISC_PATH)
+    import fileUtil
+    if (os.path.exists(FILE_LIST) and fileUtil.file_lines(FILE_LIST) == no_files):
+        # file list exists and contains correct number of lines
         with open(FILE_LIST, 'r') as f:
             for line in f:
                 line = line.replace('\n', '')
@@ -106,107 +73,55 @@ def get_code_files():
                 for ext in file_extensions:
                     if entry.endswith(ext):
                         entry = os.path.join(root, entry)
-                        entry = entry.replace('\\', '/')
                         result.append(entry)
-
-        result = sort_logically(result)
+        import masterUtil
+        result = masterUtil.sort_logically(result)
         with open(FILE_LIST, 'w') as f:
             for res in result:
-                if result.index(res) == len(result) - 1:
                     f.write(res)
-                else:
-                    f.write(res + '\n')
     return result
 
 
-def check_problem_links():
+def get_write_problem_link(file_path, index):
     """
-    Reads problem link from each code file
-    :returns: list of links, errors
-    """
-    file_paths = get_code_files()
-    links = list()
-    errors = list()
-
-    for file_path in file_paths:
-        index = file_paths.index(file_path)
-        link, success = get_problem_link(file_path, index)
-        if success == True:
-            links.append(link)
-        else:
-            errors.append(file_path)
-    if len(errors) == len(file_paths):
-        raise Exception("Fetching the links failed for all problems")
-    return links, errors
-
-
-def write_link_to_file(link, file_path):
-    """
-    Write link to file
-    """
-    file_lines = list()
-    line_no = 2 # line of file to write link to (default 2)
-    try:
-        with open(file_path, 'w') as f:
-            for line in f:
-                file_lines.append(line)
-            link = '#' + link
-            file_lines.insert(line_no - 1, link) # insert link into second line
-            for line in file_lines:
-                f.write(line + "\n")
-    except IOError:
-        raise IOError(
-            "IOError occured while writing gathered URL from website to file")
-
-
-def get_problem_link(file_path, index):
-    """
-    Gets the problem link from a HackerRank code file
+    Get the problem link for a HackerRank code file and writes it to it if not present
+    :string file_path: file path to check
+    :int index: index of code file
     :returns: link, success (link is None and success is false if link cannot be found)
     """
-    lines = []
-    file_lines = list()
-    if file_lines(file_path) > 1 and open_replace_newlines() != None:
-        lines = open_replace_newlines(file_path)
+    import_module(MISC_PATH)
+    import fileUtil
+    lines = fileUtil.read_file_to_list(file_path, True)
+    if len(lines) > 1:
         first_line = lines[0]
-        if first_line == '#!/bin/python3':
+        if first_line == '#!/usr/bin/env python3':
             if not lines[1].startswith('#https://www.hackerrank.com/'):
-                link, success = get_problem_link_URL(index)
+                link, success = get_problem_link_HTML(index, file_path)
                 if success is True:
-                    write_link_to_file(link, file_path)
+                    write_link = '#' + link
+                    write_string_to_file(file_path, write_link, LINK_LINE_NUMBER)
+                return link, success
+            else: # first line is shebang, second line is link
+                link = lines[1]
+                success = True
                 return link, success
         else:  # first line is not a shebang
-            import_module(FILEUTIL_PATH)
+            import_module(MISC_PATH)
             import fileUtil
             fileUtil.write_shebang(file_path, 3)
-            if lines[1].startswith('#https://www.hackerrank.com/'): # second line is a shebang
+            if lines[1].startswith('#https://www.hackerrank.com/'): # second line is a link
                 link = lines[1]
                 link = link[1:]  # remove comment sign ('#') from link
                 success = True
                 return link, success
             else:  # second line of file contains no link
-                link, success = get_problem_link_URL(file_path)
+                link, success = get_problem_link_HTML(index, file_path)
                 if success is True:
-                    write_link_to_file(link, file_path)
+                    write_link = '#' + link
+                    write_string_to_file(file_path, write_link, LINK_LINE_NUMBER)
                 return link, success
     else:  # file has only one line
         link = None
         success = False
         return link, success
 
-
-def open_replace_newlines(file_path):
-    """
-    Opens a file and returns list of lines
-    :returns: list of lines or None if error occured
-    """
-    lines = list()
-    try:
-        with open(file_path, 'r') as f:
-            for line in f:
-                lines.append(line)
-        for line in lines:
-            lines[lines.index(line)] = line.replace('\n', '')
-    except IOError:
-        return None
-    return lines
