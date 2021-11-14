cbe5f18546d25f8b7f1e18902a64b3cb30ac8441
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-11-14 18:23:46 +0100

    added variables, delete test content

--- HackerRank/global_vars.py
+++ HackerRank/global_vars.py
@@ -4,13 +4,15 @@ FOLDER = os.path.dirname(os.path.abspath(__file__))
 PROBLEMS = FOLDER + os.path.sep + 'problems' + os.path.sep
 DATAPATH = FOLDER + os.path.sep + 'items.csv'
 FILE_LIST = FOLDER + os.path.sep + 'file_list.txt'
+SHEBANG = '#!/usr/bin/env python3'
 # what date should be set as solved date
 DATE = 'first commit'
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 DIFFICULTY_PROMPT = '# Difficulty: '
+FILE_LINK_BEGINNING = '#https://www.hackerrank.com/'
 
-GET_WRITE_PROBLEM_LINK_TIMES = [2, 3, 7, 8]
-GET_DOMAINS_TIMES = [2, 3, 7, 8]
-GET_DIFFICULTY_TIMES = [2, 3, 7, 8]
-GET_SOLVED_DATE_TIMES = [2, 3, 7, 8]
-GET_INSTRUCTIONS_TIMES = [2, 3, 7, 8]
+GET_WRITE_PROBLEM_LINK_TIMES = list()
+GET_DOMAINS_TIMES = list()
+GET_DIFFICULTY_TIMES = list()
+GET_SOLVED_DATE_TIMES = list()
+GET_INSTRUCTIONS_TIMES = list()
