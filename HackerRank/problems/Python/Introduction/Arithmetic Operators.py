4b3e47c0d9ac5d68446b86875660e59c8491d3a5
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-11-15 15:31:37 +0100

    initial commit

---
+++ HackerRank/problems/Python/Introduction/Arithmetic Operators.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+# Difficulty: Easy
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    int_sum = a + b
+    difference = a - b
+    product = a * b
+    print(int_sum)
+    print(difference)
+    print(product)
