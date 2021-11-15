2a8967266a3ef32f36a847af964acdca4d61812d
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-11-15 10:18:26 +0100

    Update and rename A13_NegativeValsIterator.java to A13NegativeValsIterator.java

--- Sprechen Sie Java/ws-SprechenSieJava/src/chapter7/A13NegativeValsIterator.java
+++ Sprechen Sie Java/ws-SprechenSieJava/src/chapter7/A13NegativeValsIterator.java
@@ -1,6 +1,6 @@
 package chapter7;
 
-public class A13_NegativeValsIterator {
+public class A13NegativeValsIterator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
