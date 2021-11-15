f02df9eb8cf388cbca83f616357603da8ca0254c
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-11-15 16:10:44 +0100

    initial commit

---
+++ HackerRank/problems/Python/Introduction/Python Division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#https://www.hackerrank.com/challenges/python-division/problem
+# Difficulty: Easy
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    int_division = a // b
+    float_division = a / b
+    print(int_division)
+    print(float_division)
