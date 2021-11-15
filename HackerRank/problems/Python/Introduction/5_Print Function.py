7f4d83df8bd1a1aca3a752e9aa91a857bb83fd7d
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-11-15 16:51:11 +0100

    initial commit

---
+++ HackerRank/problems/Python/Introduction/5_Print Function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#https://www.hackerrank.com/challenges/python-print/problem
+# Difficulty: Easy
+
+if __name__ == '__main__':
+    n = int(input())
+    print_string = '123'
+    if n > 3:
+        for i in range(4, n + 1):
+            print_string += str(i)
+    print(print_string)
