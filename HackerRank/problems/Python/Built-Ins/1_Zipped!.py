d373d862b7962ce9f33ca43c01a2d2da534a815b
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-11-15 19:42:41 +0100

    initial commit

---
+++ HackerRank/problems/Python/Built-Ins/1_Zipped!.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#https://www.hackerrank.com/challenges/zipped/problem
+# Difficulty: Easy
+
+total_sub, total_student = input().split()
+
+all_marks = []
+
+for i in range(int(total_student)):
+    all_marks.append([float(i) for i in input().split()])
+
+for i in list(zip(*all_marks)):
+    a = (sum(i)) / int(total_student)
+    print(round(a, 1))
