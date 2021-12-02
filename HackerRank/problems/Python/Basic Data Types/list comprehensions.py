b8f63519e932ddc4f5f14c884079cc964bbb1930
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-12-02 19:56:25 +0100

    correction, simplification

--- HackerRank/problems/Python/Basic Data Types/list comprehensions.py
+++ HackerRank/problems/Python/Basic Data Types/list comprehensions.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
 
-def list_comprehensions(x, y, z, n):
-    array_list = list()
-    array_list.append([[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if ((i+j+k) != n)])
-    print(array_list)
-    
-
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-    list_comprehensions(x, y, z, n)
+    x, y, z, n = (int(input()) for _ in range(4))
+    print([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
