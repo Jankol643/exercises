941ffbf0cbe325aacfc7396f9c6de00b05e6909a
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-12-07 10:48:49 +0100

    deduplication, added func to get html difficulty

--- HackerRank/internet.py
+++ HackerRank/internet.py
@@ -3,21 +3,18 @@ from bs4 import BeautifulSoup  # for getting HTML elements of website
 
 from global_vars import HTML_FOLDER
 
-def get_HTML_path(file_path):
+def get_HTML_path(domain, subdomain):
     # get correct HTML file
-    splitted_path = file_path.split(os.path.sep)
-    domain = splitted_path[-3]
-    subdomain = splitted_path[-2] # penultimate element in file path
-    if domain == 'Tutorials' and subdomain == '30 days of Code':
+    if domain == 'Tutorials':
         html_file_path = os.path.join(HTML_FOLDER, domain, subdomain + ".html")
     else:
         html_file_path = os.path.join(HTML_FOLDER, domain + ".html")
     for path in os.listdir(HTML_FOLDER):
         full_path = os.path.join(HTML_FOLDER, path)
         if os.path.isfile(full_path):
-            if path.endswith('.html'):
-                if path == html_file_path:
-                    html_file_path = full_path
+            if full_path.endswith('.html'):
+                if full_path == html_file_path:
+                    print("Found html", html_file_path)
                     return html_file_path
     return None
 
@@ -45,10 +42,17 @@ def get_problem_link_HTML(index, file_path):
         soup = BeautifulSoup(open_html_file, 'html.parser')
         challenge_list_items = soup.find_all('a', attrs={'class': 'challenge-list-item'})
         challenge_titles = soup.find_all(class_='challengecard-title')
+        challenge_titles_list = []
+        for item in challenge_titles:
+            challenge_titles_list.append(item.find(text=True, recursive=False))
+        filename_without_ext = filename.split('.')[0]
         for item in challenge_list_items:
             item_index = challenge_list_items.index(item)
-            file_name_html = challenge_titles[item_index].string
-            if filename.lower() in file_name_html.lower():
+            file_name_html = challenge_titles_list[item_index]
+            chars_to_replace = [':']
+            for char in chars_to_replace:
+                file_name_html.replace(char, '')
+            if filename_without_ext.lower() in file_name_html.lower():
                 link = challenge_list_items[item_index]['href']
                 break
 
@@ -63,25 +67,33 @@ def get_problem_link_HTML(index, file_path):
     else: # HTML file not found
         raise FileNotFoundError("Corresponding HTML file for " + file_path + " wasn´t found.")
 
-def get_domains(file, soup):
+def get_domains(file):
     """
     Gets the domain hierarchy of a HackerRank problem (maximum depth = 3)
-    :returns: domain, subdomain, subsubdomain
+    :returns: domain, subdomain
     """
     print("Get domains for file " + file + "...")
     domain_list = list()
-    spans = soup.find_all(
-        'span', attrs={'class': 'breadcrumb-item-text'})
-    for span in spans:
-        domain_list.append(span.get_text(strip=True))
-    domain = domain_list[0]
-    if len(domain_list) > 1:
-        subdomain = domain_list[1]
-        if len(domain_list) > 2:
-            subsubdomain = domain_list[2]
-        else: # only domain and subdomain present
-            subsubdomain = None
+    domain_list.append(file.split(os.path.sep))
+    domain = domain_list[2]
+    if len(domain_list) > 3:
+        subdomain = domain_list[3]
     else: # only domain present
         subdomain = None
-        subsubdomain = None
-    return domain, subdomain, subsubdomain
+    return domain, subdomain
+
+def get_html_difficulty(file, soup):
+    """
+    Gets the difficulty of a HackerRank problem from the corresponding HTML
+
+    :param file: file path of problem file
+    :type file: string
+    :param soup: HTML
+    :type soup: beautifulsoup object
+    """
+    # Extract problem title from filename
+    title = file.split(os.path.sep)[-1].split('.')[0]
+    div = soup.find_all('div', attrs={'class': 'card-details'})[index]
+    first_child = next(div.children, None)
+    html_difficulty = first_child.text.strip()    
+    return html_difficulty
